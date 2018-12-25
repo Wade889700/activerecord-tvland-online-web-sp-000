2c8c0e89c835df05dcbc73aee34f14eed1458c94
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-25 01:15:50 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,8 +1,8 @@
 class Actor < ActiveRecord::Migration
   def change
     create_table :actors do |t|
-      t.string :firstt_name
+      t.string :first_name
       t.string :last_name
+    end
   end
-
 end
