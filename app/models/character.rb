5dfa62efe3cdd4e1f472607617d3cbcead35cd67
Hsu-Ting Kao <severus889700@gmail.com>
2018-12-25 01:46:39 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,8 +1,9 @@
 class Character < ActiveRecord::Base
-  belongs_to :actor 
+  belongs_to :actor
   belongs_to :show
 
   def say_that_thing_you_say
+    "#{urkel.name} always says: #{urkel.catchphrase}"
   end
-  
+
 end
